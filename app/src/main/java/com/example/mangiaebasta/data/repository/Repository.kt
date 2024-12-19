9da1ccfce95c1d98c437b08749a28744e7c081e3
giorgio-dal-santo <giorgio.dalsanto@studenti.unimi.it>
2024-12-19 11:06:08 +0100

    aggiunti alcuni file

---
+++ app/src/main/java/com/example/mangiaebasta/data/repository/Repository.kt
@@ -0,0 +1,7 @@
+package com.example.mangiaebasta.data.repository
+
+// data un'immagine richiesta il repository si occupa di recuperarla dal db locale o dal server
+
+class Repository {
+
+}
---
+++ app/src/main/java/com/example/mangiaebasta/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.mangiaebasta.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.example.mangiaebasta.data.repository.Repository
+
+class MainViewModel(repository: Repository): ViewModel() {
+
+}

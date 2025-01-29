f18a7e587fe4d0e2aaf2e1692ae949ce80f0c900
Carlotta Donato <carlotta.donato03@gmail.com>
2025-01-29 17:05:42 +0100

    ok definitivamente location

--- app/src/main/java/com/example/mangiaebasta/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mangiaebasta/view/screens/HomeScreen.kt
@@ -1,5 +1,15 @@
 package com.example.mangiaebasta.view.screens
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -8,8 +18,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import com.example.mangiaebasta.model.dataClasses.Error
+import com.example.mangiaebasta.view.styles.buttonTextWhiteStyle
+import com.example.mangiaebasta.view.styles.buyButtonModifier
+import com.example.mangiaebasta.view.styles.enableLocationButtonModifier
+import com.example.mangiaebasta.view.utils.ErrorDialog
+import com.example.mangiaebasta.view.utils.button.StyledButton
 import com.example.mangiaebasta.view.utils.cards.MenuCardWithButton
 import com.example.mangiaebasta.viewmodel.MainViewModel
+import com.google.android.gms.location.LocationCallback
 
 @Composable
 fun HomeScreen(
@@ -22,7 +42,11 @@ fun HomeScreen(
     val menusState by viewModel.menusExplorationState.collectAsState()
     val locationState by viewModel.locationState.collectAsState()
 
-    LaunchedEffect(menusState.reloadMenus, appState.isLoading, locationState.lastKnownLocation) {
+    if (!locationState.isLocationAllowed) {
+        return Text("NO LOCATION ALLOWED, GO TO SETTINGS")
+    }
+
+    LaunchedEffect(menusState.reloadMenus, appState.isLoading, locationState.lastKnownLocation, locationState.isLocationAllowed) {
         if (appState.isLoading) return@LaunchedEffect
         if (menusState.nearbyMenus.isEmpty() || menusState.reloadMenus) {
             viewModel.fetchNearbyMenus()
@@ -46,4 +70,18 @@ fun HomeScreen(
 
 
     }
+}
+
+fun showSettingsDialog(context: Context) {
+    AlertDialog.Builder(context)
+        .setTitle("Location services are disabled")
+        .setMessage("Please enable location services to use this feature.")
+        .setPositiveButton("Go go Settings") { _, _ ->
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", context.packageName, null)
+            }
+            context.startActivity(intent)
+        }
+        .setNegativeButton("Cancel", null)
+        .show()
 }
